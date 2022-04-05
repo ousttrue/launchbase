34b8e0a38a59d1fb2d4293b2666a7fef6e6b1e4a
ousttrue <ousttrue@gmail.com>
2022-04-06 01:23:38 +0900

    fix main

--- launchbase/__main__.py
+++ launchbase/__main__.py
@@ -3,10 +3,14 @@ import asyncio
 import prompt_toolkit
 
 
-async def main():
+async def launch():
     app = prompt_toolkit.Application(full_screen=True)
     return await app.run_async()
 
 
+def main():
+    asyncio.run(launch())
+
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
